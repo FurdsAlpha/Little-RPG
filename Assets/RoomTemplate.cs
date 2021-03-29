62ba4ba446d7074cd4daec228630af05bed05930
Pierre-Alexandre <furdsalpha@gmail.com>
2021-03-29 21:25:08 +0200

    commit de sureté

---
+++ Assets/RoomTemplate.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTemplate : MonoBehaviour
+{
+    public GameObject[] DownRoom;
+    public GameObject[] UpRoom;
+    public GameObject[] LeftRoom;
+    public GameObject[] RightRoom;
+}
