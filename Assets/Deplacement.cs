c541afb8c1d9467fae794d6296e32f57a4122b51
FurdsAlpha <73888711+FurdsAlpha@users.noreply.github.com>
2021-06-06 16:44:27 +0200

    A* PathFinding

---
+++ Assets/Deplacement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deplacement : MonoBehaviour
+{
+    public GameObject goUpButton;
+    public GameObject goRightButton;
+    public GameObject goLeftButton;
+    public GameObject goDownButton;
+
+    private Transform _other;
+    // Recuperer les salle via RoomsTemplate
+    //Pour chaque salle de x Derection ajouter le bouton corespondant
+
+    public void GoUpButton()
+    {
+        Instantiate(goUpButton, _other.position + new Vector3 (0,3,0), Quaternion.identity);
+    }
+    
+    public void GoRightButton()
+    {
+        Instantiate(goRightButton, _other.position + new Vector3 (0,7,0), Quaternion.identity);
+    }
+    
+}
