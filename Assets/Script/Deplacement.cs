cec6e0b642609300dca179dd90b2c4d37a37aa24
FurdsAlpha <73888711+FurdsAlpha@users.noreply.github.com>
2021-06-10 00:01:27 +0200

    Commit de surreté

---
+++ Assets/Script/Deplacement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Deplacement : MonoBehaviour
+{
+
+    public GameObject adversair;
+    public GameObject Player;
+    public GameObject poinDAncrage;
+    public float direction;
+    public LineRenderer ligne;
+
+
+    private void Start()
+    {
+        poinDAncrage.transform.position = adversair.transform.position + Player.transform.position;
+        poinDAncrage.transform.position = poinDAncrage.transform.position / 2;
+    }
+
+    void Update()
+    {
+        // Spin the object around the target at 20 degrees/second.
+        transform.RotateAround(poinDAncrage.transform.position, new Vector3(0,0,direction), 20 * Time.deltaTime);
+        SetLignePosition();
+        //Debug.Log(Move.ToString());
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(Player.transform.position, adversair.transform.position);
+    }
+
+    public void SetLignePosition()
+    {
+        ligne.SetPosition(0, Player.transform.position);
+        ligne.SetPosition(1, adversair.transform.position);
+        Debug.Log("La ligne a corectement été positioné");
+    }
+    public void OnMove(CombatControl inputActions)
+    {
+        Debug.Log("oui");
+    }
+}
---
+++ Assets/Script/Input/CombatControl.cs
@@ -0,0 +1,154 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Script/Input/CombatControl.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @CombatControl : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @CombatControl()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""CombatControl"",
+    ""maps"": [
+        {
+            ""name"": ""Player"",
+            ""id"": ""4da8a750-1938-49ad-97ab-964adeb0fc80"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""Button"",
+                    ""id"": ""2518d8d8-28d2-449e-ab4b-4a154c10f1b9"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""ZQSD"",
+                    ""id"": ""51ed02c5-36b2-467b-b602-f1615e5e2bc1"",
+                    ""path"": ""1DAxis"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""negative"",
+                    ""id"": ""94c63fbd-0d20-4ab7-8700-a97a0769d62a"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""positive"",
+                    ""id"": ""d94a545b-62d6-4778-9c5c-9c32d58e622b"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Player
+        m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
+        m_Player_Move = m_Player.FindAction("Move", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Player
+    private readonly InputActionMap m_Player;
+    private IPlayerActions m_PlayerActionsCallbackInterface;
+    private readonly InputAction m_Player_Move;
+    public struct PlayerActions
+    {
+        private @CombatControl m_Wrapper;
+        public PlayerActions(@CombatControl wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_Player_Move;
+        public InputActionMap Get() { return m_Wrapper.m_Player; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerActions instance)
+        {
+            if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+            }
+            m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+            }
+        }
+    }
+    public PlayerActions @Player => new PlayerActions(this);
+    public interface IPlayerActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+    }
+}
