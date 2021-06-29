0ab2fa9312fda0d395287eff4b83b0258e2844c9
FurdsAlpha <73888711+FurdsAlpha@users.noreply.github.com>
2021-06-29 15:39:42 +0200

    Final build avant l'oral

---
+++ Assets/Script/MainMenuFounctions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuFounctions : MonoBehaviour
+{
+    public string GameScene;
+    public void Start()
+    {
+    }
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+    public void Play()
+    {
+        SceneManager.LoadScene(GameScene);
+    }
+}
---
+++ Assets/Script/pauseScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pauseScript : MonoBehaviour
+{
+    public GameObject menu;
+    public bool isPaused = false;
+    public void OnPause()
+    {
+        Pause();
+    }
+
+    public void Pause()
+    {
+        if (isPaused)//Si le jeu est deja en pause
+        {
+            menu.SetActive(false);//Désactive L'ui du menu pause
+            isPaused = false;//Enregistre que le jeu n'est plus en pause
+            Time.timeScale = 1;//Le temp s'ecoule normalement
+        }
+        else
+        {
+            menu.SetActive(true);//Active L'ui du menu pause;
+            isPaused = true;//Enregistre que le jeu est en pause
+            Time.timeScale = 0;//Stop le temp
+        }
+    }
+}
